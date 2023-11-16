83a7b9df427ad9819ab8e18522f3330609ab5bfa
kushal143 <67643509+kushal143@users.noreply.github.com>
2023-11-16 10:24:00 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,24 +5,47 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://blog.saginfotech.com/wp-content/uploads/2017/04/mobileshop.jpg"
                             alt="image"
                         />
                     </div>
                     <div className="md:7/12 lg:w-6/12">
                         <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                            React development is carried out by passionate developers
+                            ABOUT US
                         </h2>
                         <p className="mt-6 text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                            <strong><em>CANKA creates a culture of infinite possibilities. The deep trust and loyalty of our customers drive us to continually grow and lead innovation.</em></strong>
                         </p>
+
                         <p className="mt-4 text-gray-600">
                             Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
                             Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
                         </p>
                     </div>
+                </div>
+
+                {/* Additional Images for workers, service centers, locations, and states */}
+                <div className="flex mt-12 space-x-6">
+                    <img
+                        src="https://sangeethamobiles.net/assets/front-end/img/About_2.png"
+                        alt="Workers"
+                        className="rounded-full"
+                    />
+                    <img
+                        src="https://sangeethamobiles.net/assets/front-end/img/About_3.png"
+                        alt="Service Centers"
+                        className="rounded-full"
+                    />
+                    <img
+                        src="https://sangeethamobiles.net/assets/front-end/img/About_4.png"
+                        alt="Locations"
+                        className="rounded-full"
+                    />
+                    <img
+                        src="https://sangeethamobiles.net/assets/front-end/img/About_5.png"
+                        alt="States Covered"
+                        className="rounded-full"
+                    />
                 </div>
             </div>
         </div>
